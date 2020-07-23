f39b3b1718eeb1b48e8241d088ba30bdfcef9e2c
AgileCloudInstitute <64223536+AgileCloudInstitute@users.noreply.github.com>
2020-07-23 16:29:12 -0700

    Update destroySimpleExample.py

--- pipeline-scripts/destroySimpleExample.py
+++ pipeline-scripts/destroySimpleExample.py
@@ -1,1 +1,20 @@
 print("Hello from inside destroySimpleExample.py ")
+
+import sys  
+    
+awsPublicAccessKey=sys.argv[1] 
+awsSecretAccessKey=sys.argv[2] 
+storageAccountNameTerraformBackend=sys.argv[3] 
+terraBackendKey=sys.argv[4] 
+awsRegion=sys.argv[5] 
+DefaultWorkingDirectory=sys.argv[6] 
+
+print("awsPublicAccessKey is: ", awsPublicAccessKey)
+print("awsSecretAccessKey is: ", awsSecretAccessKey)
+print("storageAccountNameTerraformBackend is: ", storageAccountNameTerraformBackend)
+print("terraBackendKey is: ", terraBackendKey)
+print("awsRegion is: ", awsRegion)
+print("DefaultWorkingDirectory is: ", DefaultWorkingDirectory)
+
+print("Python version is: ", sys.version_info[0])  
+
