88a7a0e460a41c950200b664ab18a036ffdf388f
AgileCloudInstitute <64223536+AgileCloudInstitute@users.noreply.github.com>
2020-07-23 16:28:20 -0700

    Update createSimpleExample.py

--- pipeline-scripts/createSimpleExample.py
+++ pipeline-scripts/createSimpleExample.py
@@ -2,8 +2,18 @@ import sys
   
 print("Hello from inside createSimpleExample.py ")  
   
-firstarg=sys.argv[1]  
-print("firstarg is: ", firstarg)  
-  
+awsPublicAccessKey=sys.argv[1] 
+awsSecretAccessKey=sys.argv[2] 
+storageAccountNameTerraformBackend=sys.argv[3] 
+terraBackendKey=sys.argv[4] 
+awsRegion=sys.argv[5] 
+DefaultWorkingDirectory=sys.argv[6] 
+
+print("awsPublicAccessKey is: ", awsPublicAccessKey)
+print("awsSecretAccessKey is: ", awsSecretAccessKey)
+print("storageAccountNameTerraformBackend is: ", storageAccountNameTerraformBackend)
+print("terraBackendKey is: ", terraBackendKey)
+print("awsRegion is: ", awsRegion)
+print("DefaultWorkingDirectory is: ", DefaultWorkingDirectory)
+
 print("Python version is: ", sys.version_info[0])  
-  
