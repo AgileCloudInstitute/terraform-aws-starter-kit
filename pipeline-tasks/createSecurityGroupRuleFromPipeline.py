9e690ae8fc6165e79b38cd10573619931dfed327
AgileCloudInstitute <64223536+AgileCloudInstitute@users.noreply.github.com>
2020-09-16 16:46:23 -0700

    Update createSecurityGroupRuleFromPipeline.py

--- pipeline-tasks/createSecurityGroupRuleFromPipeline.py
+++ pipeline-tasks/createSecurityGroupRuleFromPipeline.py
@@ -10,16 +10,6 @@ print("Hello from inside createSecurityGroupRuleFromPipeline.py ")
 ###############################################################################
 ### Print vars to validate that they are imported and also obscured
 ###############################################################################
-#$(-aws-public-access-key)  
-#$(-aws-secret-access-key)  
-#$(storageAccountNameTerraformBackend)  
-#$(terra-backend-key)  
-#$(aws-region)  
-#$(System.DefaultWorkingDirectory)  
-#$(demoStorageKey)  
-#$(foundationKeyFileTF)  
-#$(sgKeyFileTF)
-
 awsPublicAccessKey=sys.argv[1]
 awsSecretAccessKey=sys.argv[2]
 storageAccountName=sys.argv[3]
@@ -43,7 +33,7 @@ print("Python version is: ", sys.version_info[0])
 
 #The following 7 need to be made into input variables	
 resourceGroupName="pipeline-resources"	
-storageContainerName="terraform-backend-aws-starter-kit"
+storageContainerName="tfcontainer"
 vpc_name="thisVPC"
 
 ####################################################################################
@@ -68,61 +58,35 @@ print("About to list contents of (DefaultWorkingDirectory)/_terraform-aws-starte
 print(*Path(pathToSecurityGroupRulesCalls).iterdir(), sep="\n")	
 
 ##########################################################################################
-### Initialize terraform and remote backend from inside the network foundation directory
+### Get Output variables from foundation  
 ##########################################################################################
-resourceGroupNameLine="    resource_group_name  = \""+resourceGroupName+"\"\n"	
-storageAccountNameTerraformBackendLine="    storage_account_name = \""+storageAccountName+"\"\n"	
-storageContainerNameLine="    container_name       = \""+storageContainerName+"\"\n" 
-terraBackendKeyLine="    key                  = \""+foundationKeyFileTF+"\"\n"
-
-# tfFileNameAndPath=dirToUseNet+"/terraform.tf"	
-# print("tfFileNameAndPath is: ", tfFileNameAndPath)	
-# print("About to write 8 lines to a file.")	
-# f = open(tfFileNameAndPath, "w")	
-# f.write("terraform {\n")	
-# f.write("  backend \"azurerm\" {\n")	
-# f.write(resourceGroupNameLine)	
-# f.write(storageAccountNameTerraformBackendLine)	
-# f.write(storageContainerNameLine)	
-# f.write(terraBackendKeyLine)	
-# f.write("  }\n")	
-# f.write("}\n")	
-# f.close()	
-
-# print("About to read the file we just wrote.")	
-# #open and read the file after the appending:	
-# f = open(tfFileNameAndPath, "r")	
-# print(f.read()) 	
-
-print("About to refresh list contents of (DefaultWorkingDirectory)/_terraform-aws-starter-kit/drop/calls-to-modules/aws-simple-network-foundation-call-to-module/")	
-print(*Path(pathToFoundationCalls).iterdir(), sep="\n")	
-
 print("About to call terraform output:  ")	
 outputCommand="terraform output" 
 depfunc.runTerraformCommand(outputCommand, pathToFoundationCalls )	
+
+##########################################################################################
+### Initialize terraform and remote backend from inside the security group rules directory
+##########################################################################################
+depfunc.createBackendConfigFileTerraform(resourceGroupName, storageAccountName, storageContainerName, sgKeyFileTF, pathToSecurityGroupRulesCalls )
+tfFileNameAndPath=pathToSecurityGroupRulesCalls+"/terraform.tf"	
+print("About to read the file we just wrote.")	
+#open and read the file after the appending:	
+f = open(tfFileNameAndPath, "r")	
+print(f.read()) 	
+
+print("About to refresh list contents of (DefaultWorkingDirectory)/_terraform-aws-starter-kit/drop/calls-to-modules/aws-simple-network-foundation-call-to-module/")	
+print(*Path(pathToSecurityGroupRulesCalls).iterdir(), sep="\n")	
+print("About to call terraform init:  ")	
+initCommand="terraform init -backend=true -backend-config=\"access_key="+demoStorageKey+"\""  	
+depfunc.runTerraformCommand(initCommand, pathToSecurityGroupRulesCalls )	
+
 
 #############################################################################
 ### Create the security group rule(s)
 #############################################################################
 #Get Vars to pass into terraform commands:
-varsFragmentSGR = ""
-varsFragmentSGR = varsFragmentSGR +  " -var=\"aws_region=" + awsRegion +"\""  
-varsFragmentSGR = varsFragmentSGR +  " -var=\"_public_access_key=" + awsPublicAccessKey +"\""  
-varsFragmentSGR = varsFragmentSGR +  " -var=\"_secret_access_key=" + awsSecretAccessKey +"\""  
-varsFragmentSGR = varsFragmentSGR +  " -var=\"vpcName=" + vpc_name +"\""  
-varsFragmentSGR = varsFragmentSGR +  " -var=\"vpcId=" + depfunc.vpc_id +"\""  
-varsFragmentSGR = varsFragmentSGR +  " -var=\"vpcCidr=" + depfunc.vpc_cidr +"\""  
-varsFragmentSGR = varsFragmentSGR +  " -var=\"sgId=" + depfunc.sg_id +"\""  
-varsFragmentSGR = varsFragmentSGR +  " -var=\"sgName=" + depfunc.sg_name +"\""  
-#aws_region  #_public_access_key  #_secret_access_key  #vpcName  #vpcId  #vpcCidr  #sgId  #sgName
-print("varsFragmentSGR is: ", varsFragmentSGR)
-applyCommandSGR = "terraform apply -auto-approve" + varsFragmentSGR
+varsSGR=depfunc.getVarsSGRFromPipeline(awsRegion, awsPublicAccessKey, awsSecretAccessKey, vpc_name, depfunc.vpc_id, depfunc.vpc_cidr, depfunc.sg_id, depfunc.sg_name )
+print("varsSGR is: ", varsSGR)
+applyCommandSGR = "terraform apply -auto-approve" + varsSGR
 print("applyCommandSGR is: ", applyCommandSGR)
-#runTerraformCommand(applyCommandSGR, pathToSecurityGroupRulesCalls)  
-
-
-#DELETE BY UNCOMMENTING THE FOLLOWING DURING DEVELOPMENT, THEN MAKE SEPARATE FILE FOR RELEASE:  
-# print("About to call terraform destroy.  ")    
-# destroyCommand="terraform destroy -auto-approve" + varsFoundation
-# runTerraformCommand(destroyCommand, subDir4 )  
-
+depfunc.runTerraformCommand(applyCommandSGR, pathToSecurityGroupRulesCalls)  
